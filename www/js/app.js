140c30d63abd8700ade4d128ba660dfe1e47bcc3
Jonathon Klem <jklem@abbadon.local>
2016-03-12 21:03:19 -0600

    Add additional debug output

--- www/js/app.js
+++ www/js/app.js
@@ -24,18 +24,9 @@ $(document).ready(function() {
 	});
 	
 	$('#toggle-flashlight').click(function() {
-		alert('Work');
 		window.plugins.flashlight.available(function(isAvailable) {
   		if (isAvailable) {
-
-		    // switch on
-		    window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
-		
-		    // switch off after 3 seconds
-		    setTimeout(function() {
-		      window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
-		    }, 3000);
-		
+				window.plugins.flashlight.toggle(); 
 		  } else {
 		    alert("Flashlight not available on this device");
 		  }
